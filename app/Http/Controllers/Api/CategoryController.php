b961f484e350d35c9dbd39978800c6ce6262aeef
Mohamed.Zayed <Zaid.MI@sakhaa.sa>
2026-03-27 18:45:41 +0200

    eccomerce application phase 4

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Category::with('products')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $category = Category::create($request->all());
+        return response()->json($category, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Category::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $category = Category::findOrFail($id);
+        $category->update($request->all());
+        return response()->json($category, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Category::destroy($id);
+        return response()->json(["message" => "Category deleted"], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
 
 Route::get('/test', function () {
     return "test";
@@ -21,8 +22,22 @@ level 3 iphone
 
 */
 
+
+
+//Product Api routes
 Route::get("/products" , [ProductController::class , "index"]);
 Route::get("/products/{id}" , [ProductController::class , "show"]);
 Route::post("/products" , [ProductController::class , "store"]);
 Route::put("/products/{id}" , [ProductController::class , "update"]);
 Route::delete("/products/{id}" , [ProductController::class , "destroy"]);
+
+
+
+
+// Category Api routes
+
+Route::get("/categories" , [CategoryController::class , "index"]);
+Route::get("/categories/{id}" , [CategoryController::class , "show"]);
+Route::post("/categories" , [CategoryController::class , "store"]);
+Route::put("/categories/{id}" , [CategoryController::class , "update"]);
+Route::delete("/categories/{id}" , [CategoryController::class , "destroy"]);
